ff5e084a1e532d8dc821ef25efa2776c08797ae6
Elvis Guy Bakunzi <guyelvisbakunzi@gmail.com>
2025-05-04 21:36:57 +0200

    Update console.js

--- console.js
+++ console.js
@@ -1,4 +1,4 @@
 // This is a commit
 console.log("This is a new line")
 console.log("This is a new line") // a second line
-onsole.log("This is a new line") 
+onsole.log("This is a new line") // third line
